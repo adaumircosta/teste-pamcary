ce3698666421c665dc9902e2bd6ca0ef4cbad28b
Adaumir Costa <adaumircosta@gmail.com>
2019-08-29 11:06:55 -0400

    Corrigindo configurações

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-docker build -t docker build -t registry.gitlab.com/adaumir-testes/pamcary/ws-pamcary-client .
+docker build -t registry.gitlab.com/adaumir-testes/pamcary/ws-pamcary-client:0.0.1 .
 
-docker push registry.gitlab.com/adaumir-testes/pamcary/ws-pamcary-client .
+docker push registry.gitlab.com/adaumir-testes/pamcary/ws-pamcary-client:0.0.1
 
 
